7b6546cd953ce14e651da679865e5392a8722f4e
Eigil Rischel <ayegill@gmail.com>
2024-05-25 16:36:47 +0100

    Build

--- flake.nix
+++ flake.nix
@@ -1,30 +1,110 @@
 {
   inputs = {
-    forester = {
-      url = "sourcehut:~jonsterling/ocaml-forester";
-    };
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    systems.url = "github:nix-systems/default";
+    forester.url = "sourcehut:~jonsterling/ocaml-forester";
+    forest-server.url = "github:kentookura/forest-server";
   };
-  outputs =
-    { self
-    , flake-utils
-    , nixpkgs
-    , forester
-    }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        legacyPackages = import forester.legacyPackages.${system};
-      in
-      {
-        devShell =
-          pkgs.mkShell {
-            buildInputs = with pkgs; [
-              forester.packages.${system}.default
-              texlive.combined.scheme-full
-              fswatch
-            ];
-          };
-      });
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    systems,
+    forester,
+    forest-server,
+  }:
+    flake-utils.lib.eachSystem (import systems)
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      forester-pkg = forester.packages.${system}.default;
+      default-tree = "efr";
+      tlDist = pkgs.texliveFull;
+    in {
+      packages = flake-utils.lib.flattenTree rec {
+        forester = forester-pkg;
+        tldist = tlDist;
+        new = pkgs.writeScriptBin "new"
+        ''
+          ${forester-pkg}/bin/forester new --dirs=trees --dest=trees --prefix=$1
+        '';
+        build = pkgs.writeScriptBin "build"
+        ''
+          ${forester-pkg}/bin/forester build --dev --root ${default-tree}-0001 trees/
+        '';
+        serve = pkgs.writeScriptBin "serve"
+        ''
+          ${pkgs.python3}/bin/python3 -m http.server -d output 8080
+        '';
+        forester-dev = pkgs.writeScriptBin "forester-dev"
+        ''
+          ${forest-server.packages.${system}.default}/bin/forest watch $@ -- "build forest.toml --dev"
+        '';
+        forest = pkgs.stdenv.mkDerivation {
+          name = "eigil-forest-xml";
+          src = ./.;
+          buildInputs = [
+            tlDist
+            forester-pkg
+          ];
+          buildPhase = ''
+            forester build --root ${default-tree}-0001 trees/
+            mv output/ $out/
+            cp pdfbuilds.json $out/
+          '';
+        };
+        pdfbuilds = pkgs.stdenv.mkDerivation {
+          name = "eigil-forest-pdfs";
+          unpackPhase = "true";
+          buildInputs = [
+            tlDist
+            pkgs.jq
+          ];
+          buildPhase = ''
+            mkdir -p $out/
+            mkdir latex/
+            while read build
+            do
+              tree=$(echo "$build" | jq -r .tree)
+              style=$(echo "$build" | jq -r .style)
+              ${pkgs.saxon-he}/bin/saxon-he "-s:${forest}/$tree.xml" "-xsl:${forest}/$style.xsl" "-o:$tree.tex"
+              pdflatex "$tree.tex"
+              cp "$tree.pdf" "$out/"
+            done < <(cat ${forest}/pdfbuilds.json | jq -c '.[]')
+          '';
+        };
+        default = pkgs.stdenv.mkDerivation {
+          name = "eigil-forest";
+          unpackPhase = "true";
+          buildPhase = ''
+            mkdir -p $out/
+            cp -r ${forest}/* $out/
+            cp -r ${pdfbuilds}/* $out/
+          '';
+        };
+      };
+
+      devShells.shell-minimal = pkgs.mkShell {
+        buildInputs = with pkgs; with self.packages.${system}; [
+          forester-pkg new build serve
+        ];
+      };
+
+      devShells.shell-notex = pkgs.mkShell {
+        buildInputs = with pkgs; with self.packages.${system}; [
+          forester-pkg new build serve forester-dev
+        ];
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; with self.packages.${system}; [
+          forester-pkg new build serve
+          forester-dev
+          forest-server.packages.${system}.default
+          tlDist
+        ];
+      };
+    });
 }
