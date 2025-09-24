5d519e5e7a32dd7bf2b79ee83e09eac69498916e
Eigil Rischel <ayegill@gmail.com>
2025-09-24 16:39:59 +0300

    Add website deploy script (in progress)

---
+++ deploy_website.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+TARGET=root@172.105.82.133:/var/www/erischel.com/html
+
+git diff --quiet || {echo 'Unstaged changes detected, are you use you want to proceed' >&2; exit 1;} 
+
+echo "Now doing automated commit"
+
+git commit -m "automated commit - deploying website"
+git pull --rebase
+git push
+
+echo "Now building forest"
+
+./forester build public-forest.toml
+
+echo "Replacing theme with public theme"
+
+mv -f public-theme/* output/
+
+echo "Now publishing website"
+
+rsync -r public/ root@172.105.82.133:/var/www/erischel.com/html
